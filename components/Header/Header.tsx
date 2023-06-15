1cbe6e3b82f30409e46ef4f080d39ca858630916
Jahid0178 <zahidul.haque767@gmail.com>
2023-06-15 08:38:52 +0600

    mod - header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import Button from "../Buttons/Button";
 import Logo from "./Logo/Logo";
 import { FaBars } from "react-icons/fa";
+import { BsBag } from "react-icons/bs";
 
 const Header = () => {
   return (
@@ -12,9 +13,12 @@ const Header = () => {
             <FaBars size={25} className="cursor-pointer" color="#fff" />
           </span>
           <Logo src="https://assets.website-files.com/632b356cbea6db8b490879b2/636e61a6e880a28a49265c86_Asset%203.svg" />
-          <span>
+          <span className="flex gap-4">
             <Button className="btn btn-primary" type="button">
               Shop Now
+            </Button>
+            <Button className="btn btn-secondary" type="button">
+              <BsBag size={20} />
             </Button>
           </span>
         </nav>
