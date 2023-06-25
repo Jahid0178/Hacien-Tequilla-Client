dcbf37ef8872ea12ce558ed8eed7f52f9b763e98
Jahid0178 <zahidul.haque767@gmail.com>
2023-06-25 14:41:26 +0600

    add - all events in data file and showing all events in events page

---
+++ components/Cards/EventCard.jsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Image from "next/image";
+import { FaCalendarAlt, FaClock, FaLocationArrow } from "react-icons/fa";
+
+const EventCard = ({ data }) => {
+  const { imageSrc, name, date, time, location } = data;
+  return (
+    <div className="bg-[#2B2A2A] h-full rounded-md overflow-hidden border border-transparent hover:border-[#988357] transition-all ease-in-out delay-100">
+      <Image
+        src={imageSrc}
+        alt={name}
+        width={350}
+        height={280}
+        className="w-full h-60 object-cover"
+      />
+      <div>
+        <ul className="p-6">
+          <li className="mb-2">
+            <h2 className="text-2xl">{name}</h2>
+          </li>
+          <li className="mb-2">
+            <p className="flex gap-4 items-center">
+              <FaCalendarAlt color="#988357" /> {date}
+            </p>
+          </li>
+          <li className="mb-2">
+            <p className="flex gap-4 items-center">
+              <FaClock color="#988357" /> {time}
+            </p>
+          </li>
+          <li className="mb-2">
+            <p className="flex gap-4 items-center">
+              <span>
+                <FaLocationArrow color="#988357" />
+              </span>
+              {location}
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default EventCard;
